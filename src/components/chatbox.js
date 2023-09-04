149a38ec00fb6d29b6575d78c9d7d40c190afc68
Anirban <reach@anirbansinha.in>
2023-09-05 02:45:06 +0530

    chat boxes ready

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -19,12 +19,52 @@ export default function Chatbox() {
           <FontAwesomeIcon icon={faTimes} />
         </div>
       </div>
+      {/*Introductory Message*/}.
       <div className="chatbox-body">
+        <div className="chatbox-body-reply-message">
+          <div className="chatbox-body-message-avatar"></div>
+          <p className=".chatbox-body-reply-message-content-text">
+            Hi, I'm Pam. How can I help you?
+          </p>
+        </div>
+        {/*---------------Loop Starts-----------------*/}.
         <div className="chatbox-body-messages">
-          <div className="chatbox-body-message">
-            <p>Hi, I'm Pam. How can I help you?</p>
+          <div className="chatbox-body-question-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p>Can you please tell me my name?</p>
+          </div>
+          <div className="chatbox-body-reply-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p className=".chatbox-body-reply-message-content-text">
+              Your name is Anirban Sinha
+            </p>
           </div>
         </div>
+        <div className="chatbox-body-messages">
+          <div className="chatbox-body-question-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p>What is my Role?</p>
+          </div>
+          <div className="chatbox-body-reply-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p className=".chatbox-body-reply-message-content-text">
+              Your Role is SDE 3
+            </p>
+          </div>
+        </div>
+        <div className="chatbox-body-messages">
+          <div className="chatbox-body-question-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p>Who is my Manager?</p>
+          </div>
+          <div className="chatbox-body-reply-message">
+            <div className="chatbox-body-message-avatar"></div>
+            <p className=".chatbox-body-reply-message-content-text">
+              Jagadish Chandra Bose
+            </p>
+          </div>
+        </div>
+        {/*---------------Loop Ends-----------------*/}.
         <div className="chatbox-body-input">
           <input type="text" placeholder="Type your message here..." />
           <button>
