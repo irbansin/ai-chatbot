9fb6fa696d123bd040a02e45f9ed297a190de779
Anirban <reach@anirbansinha.in>
2023-09-05 05:32:17 +0530

    deleted commented code

--- src/components/chatbox/chatbox-body/chatbox-body.js
+++ src/components/chatbox/chatbox-body/chatbox-body.js
@@ -11,7 +11,6 @@ export default function ChatboxBody(props) {
           Hi, I'm Pam. How can I help you?
         </p>
       </div>
-      {/*---------------Loop Starts-----------------*/}.
       {props.messageList.map((message, index) => {
         return (
           <div key={index}>
@@ -35,48 +34,6 @@ export default function ChatboxBody(props) {
           </div>
         );
       })}
-      {/* <div className="chatbox-body-messages">
-          <div className="chatbox-body-question-message">
-            <div className="chatbox-body-message-avatar">
-              {" "}
-              <div className="user"></div>
-            </div>
-            <p>Can you please tell me my name?</p>
-          </div>
-          <div className="chatbox-body-reply-message">
-            <div className="chatbox-body-message-avatar">
-              <div className="bot"></div>
-            </div>
-            <p className=".chatbox-body-reply-message-content-text">
-              Your name is Eva
-            </p>
-          </div>
-        </div> */}
-      {/*---------------Loop Ends-----------------*/}.
-      {/* <div className="chatbox-body-messages">
-          <div className="chatbox-body-question-message">
-            <div className="chatbox-body-message-avatar"></div>
-            <p>What is my Role?</p>
-          </div>
-          <div className="chatbox-body-reply-message">
-            <div className="chatbox-body-message-avatar"></div>
-            <p className=".chatbox-body-reply-message-content-text">
-              Your Role is SDE 3
-            </p>
-          </div>
-        </div>
-        <div className="chatbox-body-messages">
-          <div className="chatbox-body-question-message">
-            <div className="chatbox-body-message-avatar"></div>
-            <p>Who is my Manager?</p>
-          </div>
-          <div className="chatbox-body-reply-message">
-            <div className="chatbox-body-message-avatar"></div>
-            <p className=".chatbox-body-reply-message-content-text">
-              Jagadish Chandra Bose
-            </p>
-          </div>
-        </div> */}
     </div>
   );
 }
